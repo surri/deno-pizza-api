375c6ebbeab8443c60a65705bd6965c8acef08af
surri <thekeh@hotmail.com>
2022-04-29 14:34:57 +0900

    .env

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
-import { Application} from 'https://deno.land/x/oak@v6.0.1/mod.ts';
+import { Application } from 'https://deno.land/x/oak@v6.0.1/mod.ts';
+import { config } from "https://deno.land/std@0.137.0/dotenv/mod.ts";
 
+const env = await config();
+const port = Number(env.PORT) || 3000;
 const app = new Application();
 
-app.listen({ port: 3000 });
-console.log(`HTTP webserver running. Access it at: http://localhost:3000/`);
+app.listen({ port });
+console.log(`Running at http://localhost:${port}/`);
