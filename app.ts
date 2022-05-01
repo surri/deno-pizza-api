44ef9b99481a2a2fbb293032e6b237177ed66a5e
surri <thekeh@hotmail.com>
2022-05-01 18:54:23 +0900

    router

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
-import { Application } from 'https://deno.land/x/oak@v6.0.1/mod.ts';
+import { Application, Router} from 'https://deno.land/x/oak@v6.0.1/mod.ts';
 import { config } from "https://deno.land/std@0.137.0/dotenv/mod.ts";
 
 const env = await config();
 const port = Number(env.PORT) || 3000;
 const app = new Application();
+const router = new Router();
 
+router.get("/v1/ping", (context) => {
+  context.response.status = 200
+  context.response.body = {
+    success: true,
+    msg: "pong",
+  };
+});
+
+app.use(router.routes())
 app.listen({ port });
 console.log(`Running at http://localhost:${port}/`);
